b8d9fd580bcfe5d53db179aa454036ae5bf80e78
JoeyHJava <3studyemail@gmail.com>
2022-08-05 12:03:19 -0700

    AB#710 add setting screen, setting json, and logic to update Setting fields from screen

---
+++ src/main/java/com/huntersadventures/settings/Setting.java
@@ -0,0 +1,66 @@
+package com.huntersadventures.settings;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.huntersadventure.jsonparser.Json;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Setting{
+    private String name;
+    private String difficulty;
+
+
+    public Setting() {
+        loadSettings();
+    }
+
+    private void loadSettings() {
+        Json json = new Json();
+        try {
+            JsonNode settingJson = json.parse(json.getResourceStream("/settings.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Setting setting = (Setting) o;
+        return getDifficulty() == setting.getDifficulty() && Objects.equals(getName(), setting.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getDifficulty());
+    }
+
+    @Override
+    public String toString() {
+        return "Setting{" +
+                "name='" + name + '\'' +
+                ", strength=" + difficulty +
+                '}';
+    }
+
+
+}
