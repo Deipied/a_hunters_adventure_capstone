84d2e448f4d69c0349a61e75f29a28218d45ca1c
JoeyHJava <3studyemail@gmail.com>
2022-08-08 09:47:48 -0700

    AB#710 add preset values for settings and finish button to close modal

--- src/main/java/com/huntersadventures/settings/SettingJRadioButton.java
+++ src/main/java/com/huntersadventures/settings/SettingJRadioButton.java
@@ -1,5 +1,7 @@
 package com.huntersadventures.settings;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,16 +10,17 @@ import java.awt.event.ActionListener;
 public class SettingJRadioButton extends JFrame {
 
     public SettingJRadioButton(Setting setting) {
+
         JFrame frame = new JFrame("Settings");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new GridLayout(0, 1));
 
-        ButtonGroup group1 = new ButtonGroup();
+        ButtonGroup difficultyGroup = new ButtonGroup();
         JRadioButton easy = new JRadioButton(String.valueOf(SettingType.EASY));
         JRadioButton hard = new JRadioButton(String.valueOf(SettingType.HARD));
 
-        ButtonGroup group2 = new ButtonGroup();
+        ButtonGroup nameGroup = new ButtonGroup();
         JRadioButton vader = new JRadioButton(String.valueOf(SettingType.VADER));
         JRadioButton potter = new JRadioButton(String.valueOf(SettingType.POTTER));
 
@@ -44,17 +47,30 @@ public class SettingJRadioButton extends JFrame {
         JLabel difLabel = new JLabel(String.valueOf(SettingType.DIFFICULTY));
         JLabel charLabel = new JLabel(String.valueOf(SettingType.CHARACTER));
 
-        group2.add(vader);
-        group2.add(potter);
+        //preset radioButtons
+        easy.setSelected(true);
+        vader.setSelected(true);
+
+        nameGroup.add(vader);
+        nameGroup.add(potter);
+        panel.add(charLabel);
         panel.add(vader);
         panel.add(potter);
 
-        group1.add(easy);
-        group1.add(hard);
+        difficultyGroup.add(easy);
+        difficultyGroup.add(hard);
+        panel.add(difLabel);
         panel.add(easy);
         panel.add(hard);
 
-//        easy.addItemListener(itemListener);
+        //close button
+        JButton finishButton = new JButton("Finished");
+        finishButton.setSize(0, 0);
+        finishButton.addActionListener(e -> {
+            frame.dispose();
+        });
+        panel.add(finishButton);
+
         hard.addActionListener(actionListener);
         vader.addActionListener(actionListener);
         potter.addActionListener(actionListener);
@@ -64,5 +80,4 @@ public class SettingJRadioButton extends JFrame {
         frame.setSize(300, 200);
         frame.setVisible(true);
     }
-
 }
