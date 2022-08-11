dc865a232db4f00bd2bae7cc7aec01fd331595c7
JoeyHJava <3studyemail@gmail.com>
2022-08-11 10:41:19 -0700

    AB#1002 inventory view now reflects item moved from view. Inventory content is centered, background and font color inline with game

--- src/main/java/com/huntersadventure/swing/CombatInventory.java
+++ src/main/java/com/huntersadventure/swing/CombatInventory.java
@@ -14,15 +14,25 @@ public class CombatInventory {
 
         GUI.inventoryPanel.removeAll();
         JList<String> displayList = new JList<>(panelList);
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) displayList.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
         for (Item item : p1.getInventory()
         ) {
             panelList.addElement(item.getName());
         }
 
-        displayList.setBounds(100, 100, 200, 200);
+        displayList.setBounds(100, 50, 200, 200);
+        displayList.setBackground(Color.black);
+        displayList.setForeground(Color.white);
+        displayList.setFont(new Font("Serif", Font.BOLD, 20));
+//        GUI.inventoryPanel.setFont(new java.awt.Font("Tacoma", 0, 44));
 
-        GUI.inventoryPanel.setFont(new java.awt.Font("Tacoma", 0, 44));
-        GUI.inventoryPanel.add(displayList);
+        JLabel label = new JLabel("Inventory", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.BOLD, 33));
+        label.setForeground(Color.white);
+        label.setBounds(10, 70, 577, 350);
+        GUI.inventoryPanel.add(label);
+        GUI.inventoryPanel.add(displayList);;
 
     }
 
